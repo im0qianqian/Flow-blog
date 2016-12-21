9a1f9c075d1ecd5624d8cabd332925ac57a85635
千千 <qian1335661317@qq.com>
2016-12-21 15:45:58 +0800

    test

--- Pyblog/settings.py
+++ Pyblog/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'Pyblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'mysite/../templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- mysite/views.py
+++ mysite/views.py
@@ -5,13 +5,6 @@ from mysite import models
 # Create your views here.
 
 def index(request):
-    # return HttpResponse("Hello world!")
-    if request.method == "POST":
-        username = request.POST.get("username",None)
-        password = request.POST.get("password",None)
-        print (username,password)
-        models.UserInfo.objects.create(user=username,pwd=password)
-    user_list = models.UserInfo.objects.all()
-    for i in user_list:
-        print (i.user,i.pwd)
+    # return HttpResponse(a+"Hello world!"+b)
+    user_list = []
     return render(request,"index.html",{"data":user_list})
