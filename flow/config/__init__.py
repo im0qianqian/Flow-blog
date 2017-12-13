a05305ae0a48e8626b6ba0d35174a982944ecbfb
千千 <qian1335661317@qq.com>
2017-12-13 16:55:58 +0800

    评论框更换

--- flow/config/__init__.py
+++ flow/config/__init__.py
@@ -14,7 +14,7 @@ DATABASE_CONFIG = {
 
 # 每页显示最大文章数量
 MAXIMUM_OF_PAGE = 3
-SITE_URL = "http://127.0.0.1"
+SITE_URL = "http://127.0.0.1:8000"
 BLOG_TITLE = "FLOW BLOG"
 BLOG_DESCRIPTION = "Snow Memory"
 LOGO_IMAGE = "http://static.dreamwings.cn/wp-content/uploads/2016/06/10102wq.jpg"
